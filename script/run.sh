36eaebb55f7460af0183dc3995498378102b91aa
Meng-Ju Tsai <metsai@cern.ch>
2021-09-30 16:31:01 +0200

    update

--- script/run.sh
+++ script/run.sh
@@ -1,7 +1,7 @@
-python share/submit_all.py  --job ttH_mH400_wH5_schan --analysis ttH --mH2 400 --wH2 5 --joboption mc.MGPy8EG_NNPDF31_mh2_wh2_hbsm4tops_schan.py --maxEvents 100 --runNumber 999999
-python share/submit_all.py  --job ttH_mH400_wH5_stchan --analysis ttH --mH2 400 --wH2 5 --joboption mc.MGPy8EG_NNPDF31_mh2_wh2_hbsm4tops_stchan.py --maxEvents 100 --runNumber 999999
-python share/submit_all.py  --job ttH_mH400_wH5_tchan --analysis ttH --mH2 400 --wH2 5 --joboption mc.MGPy8EG_NNPDF31_mh2_wh2_hbsm4tops_tchan.py --maxEvents 100 --runNumber 999999
+python share/submit_all.py --no_sub --job ttH_mH400_wH5_schan --analysis ttH --mH2 400 --wH2 5 --joboption mc.MGPy8EG_NNPDF31_mh2_wh2_hbsm4tops_schan.py --maxEvents 100 --runNumber 999999
+python share/submit_all.py --no_sub --job ttH_mH400_wH5_stchan --analysis ttH --mH2 400 --wH2 5 --joboption mc.MGPy8EG_NNPDF31_mh2_wh2_hbsm4tops_stchan.py --maxEvents 100 --runNumber 999999
+python share/submit_all.py --no_sub --job ttH_mH400_wH5_tchan --analysis ttH --mH2 400 --wH2 5 --joboption mc.MGPy8EG_NNPDF31_mh2_wh2_hbsm4tops_tchan.py --maxEvents 100 --runNumber 999999
 
-python share/submit_all.py  --job ttA_mA400_wA5_schan --analysis ttA --mH3 400 --wH3 5 --joboption mc.MGPy8EG_NNPDF31_mh3_wh3_hbsm4tops_schan.py --maxEvents 100 --runNumber 999999
-python share/submit_all.py  --job ttA_mA400_wA5_stchan --analysis ttA --mH3 400 --wH3 5 --joboption mc.MGPy8EG_NNPDF31_mh3_wh3_hbsm4tops_stchan.py --maxEvents 100 --runNumber 999999
-python share/submit_all.py  --job ttA_mA400_wA5_tchan --analysis ttA --mH3 400 --wH3 5 --joboption mc.MGPy8EG_NNPDF31_mh3_wh3_hbsm4tops_tchan.py --maxEvents 100 --runNumber 999999
+python share/submit_all.py --no_sub --job ttA_mA400_wA5_schan --analysis ttA --mH3 400 --wH3 5 --joboption mc.MGPy8EG_NNPDF31_mh3_wh3_hbsm4tops_schan.py --maxEvents 100 --runNumber 999999
+python share/submit_all.py --no_sub --job ttA_mA400_wA5_stchan --analysis ttA --mH3 400 --wH3 5 --joboption mc.MGPy8EG_NNPDF31_mh3_wh3_hbsm4tops_stchan.py --maxEvents 100 --runNumber 999999
+python share/submit_all.py --no_sub --job ttA_mA400_wA5_tchan --analysis ttA --mH3 400 --wH3 5 --joboption mc.MGPy8EG_NNPDF31_mh3_wh3_hbsm4tops_tchan.py --maxEvents 100 --runNumber 999999
